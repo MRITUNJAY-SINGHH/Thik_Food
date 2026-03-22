1c9c39b94c5a8290b1e7446517b0aa1246468e2e
MRITUNJAY SINGH <mritunjays447@gmail.com>
2026-03-22 16:18:33 +0000

    update homepage and other changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,228 +20,250 @@ import { removeItem } from '../../store/cartSlice.js';
 import { setSearchResults } from '../../store/searchResult.js';
 
 const Header = () => {
-  const location = useLocation();
-  const [helmet, setHelmet] = useState('');
-  const [searchInput, setSearchInput] = useState('');
-  // const [category, setCategory] = useState('');
+   const location = useLocation();
+   const [helmet, setHelmet] = useState('');
+   const [searchInput, setSearchInput] = useState('');
+   // const [category, setCategory] = useState('');
 
-  const navigate = useNavigate();
+   const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-  const wishlist = useSelector((state) => state.wishlist.items.length);
-  const Compare = useSelector((state) => state.product.productItems.length);
-  const cart = useSelector((state) => state.cart.cart);
-  const subtotal = useSelector((state) => state.cart.subTotal);
+   const dispatch = useDispatch();
+   const wishlist = useSelector((state) => state.wishlist.items.length);
+   const Compare = useSelector((state) => state.product.productItems.length);
+   const cart = useSelector((state) => state.cart.cart);
+   const subtotal = useSelector((state) => state.cart.subTotal);
 
+   useEffect(() => {
+      const pathnameParts = location.pathname.split('/');
+      const currentPage = pathnameParts[pathnameParts.length - 1];
+      setHelmet(currentPage);
+   }, [location]);
 
+   const productsName = product.productData.map((item) =>
+      item.items.map((item) => item),
+   );
+   const productFilterName = productsName.flat();
 
-  useEffect(() => {
-    const pathnameParts = location.pathname.split('/');
-    const currentPage = pathnameParts[pathnameParts.length - 1];
-    setHelmet(currentPage);
-  }, [location]);
+   const handleSearch = (e) => {
+      e.preventDefault();
+      if (searchInput === '') {
+         dispatch(setSearchResults([]));
+      }
 
-
-  const productsName = product.productData.map((item) =>
-    item.items.map((item) => item)
-  );
-  const productFilterName = productsName.flat();
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    if (searchInput === '') {
-      dispatch(setSearchResults([]));
-    }
-
-    let results = [];
-    productFilterName.forEach((item) => {
-      item.products.forEach((product) => {
-        if (
-          product.productName.toLowerCase().includes(searchInput.toLowerCase())
-        ) {
-          results.push(product);
-        }
+      let results = [];
+      productFilterName.forEach((item) => {
+         item.products.forEach((product) => {
+            if (
+               product.productName
+                  .toLowerCase()
+                  .includes(searchInput.toLowerCase())
+            ) {
+               results.push(product);
+            }
+         });
       });
-    });
-    
-   dispatch(setSearchResults(results));
 
-   if (results.length > 0) {
-     navigate(`/product/${searchInput.replace(/ /g, '-').toLowerCase()}`);
-   }
-  };
+      dispatch(setSearchResults(results));
 
+      if (results.length > 0) {
+         navigate(`/product/${searchInput.replace(/ /g, '-').toLowerCase()}`);
+      }
+   };
 
+   const handleDeleteCartItem = (id) => {
+      const product = cart.find((product) => product.id === id);
+      if (product) {
+         dispatch(removeItem(product));
+      }
+   };
 
-
-  const handleDeleteCartItem = (id) => {
-    const product = cart.find((product) => product.id === id);
-    if (product) {
-      dispatch(removeItem(product));
-    }
-  };
-
-
-  return (
-    <>
-      <div className='header flex justify-between items-center mx-2 my-4 pt-[20px] pb-[30px]'>
-        <div className='logo flex justify-center items-center w-[9%]'>
-          <Link to='/'>
-            <img src={Logo} alt='logo' className='h-12 w-full' />
-          </Link>
-        </div>
-        <div className='Search  h-11 flex items-center w-[45%] mx-5 my-autocomplete'>
-          <input
-            id='search-input'
-            type='text'
-            placeholder='Search products, brands, and more'
-            className='outline-none pl-5 flex-1 dark:bg-gray-900 dark:text-white placeholder-black dark:placeholder-white'
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            onClick={handleSearch}
-            disabled={true}
-          />
-
-          <div
-            className='search-icon cursor-pointer dark:text-white'
-            // disabled="true"
-          >
-            <FaSearch />
-          </div>
-        </div>
-
-        <div className='options w-[35%] flex justify-around items-center'>
-          <div className='flex items-center justify-center'>
-            <Link to='/compare'>
-              <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
-                <IoGitCompareOutline className='h-7 w-7' />
-                <span className='text-sm font-semibold dark:text-white text-black'>
-                  Compare
-                </span>
-                <span className='absolute top-[-20px] left-2 bg-[#29a56c] dark:bg-white text-white dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
-                  {Compare}
-                </span>
-              </div>
-            </Link>
-          </div>
-
-          <div className='flex items-center justify-center'>
-            <Link to='/wishlist'>
-              <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
-                <MdFavoriteBorder className='h-7 w-7' />
-                <span className='text-sm font-semibold dark:text-white text-black'>
-                  Wishlist
-                </span>
-                <span className='absolute top-[-20px] left-0 bg-[#29a56c] dark:bg-white text-white dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
-                  {wishlist}
-                </span>
-              </div>
-            </Link>
-          </div>
-          <div className='dropdown-cart'>
-            <div className='flex items-center justify-center'>
-              <Link to='/cart'>
-                <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
-                  <AiOutlineShoppingCart className='h-7 w-7 flex justify-center items-center' />
-                  <span className='text-sm font-semibold dark:text-white text-black'>
-                    Cart
-                  </span>
-                  <span className='absolute top-[-20px] left-1 bg-[#29a56c] dark:bg-white  dark:text-black text-white rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
-                    {cart.length}
-                  </span>
-                </div>
-              </Link>
+   return (
+      <>
+         <div className='header flex justify-between items-center mx-2 my-4 pt-[20px] height-[30px]'>
+            <div className='logo flex justify-center items-center w-[9%]'>
+               <Link to='/'>
+                  <h2 className='text-black font-bold text-2xl'>TIB</h2>
+               </Link>
             </div>
+            {/* <div className='Search  h-11 flex items-center w-[45%] mx-5 my-autocomplete'>
+               <input
+                  id='search-input'
+                  type='text'
+                  placeholder='Search products, brands, and more'
+                  className='outline-none pl-5 flex-1 dark:bg-gray-900 dark:text-white placeholder-black dark:placeholder-white'
+                  value={searchInput}
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  onClick={handleSearch}
+                  disabled={true}
+               />
 
-            <div className='cart-dropdown-wrap cart-dropdown-hm2'>
-              <ul>
-                {cart.length === 0 ? (
-                  <li className='empty-cart flex flex-col items-center justify-center space-y-2 py-4'>
-                    <span className='text-lg font-semibold text-gray-700'>
-                      Your shopping cart is empty.
-                    </span>
-                    <span className='text-sm text-gray-500'>
-                      Start adding products to fill it up!
-                    </span>
+               <div
+                  className='search-icon cursor-pointer dark:text-white'
+                  // disabled="true"
+               >
+                  <FaSearch />
+               </div>
+            </div> */}
+
+            <nav>
+               <ul className='flex center justify-between gap-10 ml-8 font-semibold'>
+                  <li>
+                     <Link to='/'>Home</Link>
                   </li>
-                ) : (
-                  cart.slice(0, 2).map((item) => (
-                    <li key={item.id}>
-                      <div className='shopping-cart-img'>
-                        <img alt='Nest' src={item.catImg} />
-                      </div>
-                      <div className='shopping-cart-title'>
-                        <Link to='/cart'>
-                          <h3 className='text-sm'>{item.productName}</h3>
-                        </Link>
-                        <h4>
-                          <span>{item.quantity} × </span>₹{item.price}
-                        </h4>
-                      </div>
-                      <div className='shopping-cart-delete'>
-                        <Link
-                          to='#'
-                          onClick={() => handleDeleteCartItem(item.id)}
-                        >
-                          <FiX />
-                        </Link>
-                      </div>
-                    </li>
-                  ))
-                )}
-              </ul>
-              {cart.length > 2 && (
-                <div className='more-items-message bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 mt-2 rounded'>
-                  <span className='block'>
-                    Only the first 2 items are displayed.
-                  </span>
-                  <span className='block mt-1'>
-                    Open the cart to see all items.
-                  </span>
-                </div>
-              )}
-              {cart.length > 0 && (
-                <div className='shopping-cart-footer'>
-                  <div className='shopping-cart-total'>
-                    <h4>
-                      Total <span>₹{subtotal}</span>
-                    </h4>
-                  </div>
-                  <div className='shopping-cart-button text-center'>
-                    <Link className='outline' to='/cart'>
-                      View cart
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+                  <li>
+                     <Link to='/shop'>Shop</Link>
+                  </li>
+                  <li>
+                     <Link to='/about'>Collection</Link>
+                  </li>
+                  <li>
+                     <Link to='/contact'>New Arrivals</Link>
+                  </li>
+                  <li>
+                     <Link to='/blog'>Best Sellers</Link>
+                  </li>
+                  <li>
+                     <Link to='/blog'>Our Story</Link>
+                  </li>
+                  <li>
+                     <Link to='/blog'>Business Order</Link>
+                  </li>
+               </ul>
+            </nav>
 
-          <div className='flex justify-center item-center  relative  cursor-pointer'>
-            <DropdownMenu />
-          </div>
-          <div>
-            <DarkNightMode />
-          </div>
-        </div>
-      </div>
-      {/* Navbar section Start Here */}
-      <Navbar />
-      {/* Navbar section End Here */}
-      {location.pathname === '/' ? null : (
-        <div className='page-header breadcrumb-wrap mb-[30px]'>
-          <div className='container'>
-            <div className='breadcrumb font-normal'>
-              <Link to={'/'} rel='nofollow'>
-                <FiHome className='inline mr-1' /> Home
-              </Link>
-              <FaAngleRight /> Pages <FaAngleRight /> {helmet}
+            <div className='options w-[auto] gap-5 flex justify-around items-center'>
+               <div className='flex items-center justify-center'>
+                  {/* <Link to='/compare'>
+                     <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                        <IoGitCompareOutline className='h-7 w-7' />
+                        <span className='text-sm font-semibold dark:text-white text-black'>
+                           Compare
+                        </span>
+                        <span className='absolute top-[-20px] left-2 bg-[#29a56c] dark:bg-white text-white dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                           {Compare}
+                        </span>
+                     </div>
+                  </Link> */}
+               </div>
+
+               <div className='flex items-center justify-center'>
+                  <Link to='/wishlist'>
+                     <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                        <MdFavoriteBorder className='h-7 w-7' />
+
+                        <span className='absolute top-[-20px] left-0 bg-[#29a56c] dark:bg-white text-white dark:text-black rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                           {wishlist}
+                        </span>
+                     </div>
+                  </Link>
+               </div>
+               <div className='dropdown-cart'>
+                  <div className='flex items-center justify-center'>
+                     <Link to='/cart'>
+                        <div className='flex justify-center items-center space-x-2 relative cursor-pointer'>
+                           <AiOutlineShoppingCart className='h-7 w-7 flex justify-center items-center' />
+
+                           <span className='absolute top-[-20px] left-1 bg-[#29a56c] dark:bg-white  dark:text-black text-white rounded-full h-7 w-7 flex items-center justify-center text-xs font-bold'>
+                              {cart.length}
+                           </span>
+                        </div>
+                     </Link>
+                  </div>
+
+                  <div className='cart-dropdown-wrap cart-dropdown-hm2'>
+                     <ul>
+                        {cart.length === 0 ? (
+                           <li className='empty-cart flex flex-col items-center justify-center space-y-2 py-4'>
+                              <span className='text-lg font-semibold text-gray-700'>
+                                 Your shopping cart is empty.
+                              </span>
+                              <span className='text-sm text-gray-500'>
+                                 Start adding products to fill it up!
+                              </span>
+                           </li>
+                        ) : (
+                           cart.slice(0, 2).map((item) => (
+                              <li key={item.id}>
+                                 <div className='shopping-cart-img'>
+                                    <img alt='Nest' src={item.catImg} />
+                                 </div>
+                                 <div className='shopping-cart-title'>
+                                    <Link to='/cart'>
+                                       <h3 className='text-sm'>
+                                          {item.productName}
+                                       </h3>
+                                    </Link>
+                                    <h4>
+                                       <span>{item.quantity} × </span>₹
+                                       {item.price}
+                                    </h4>
+                                 </div>
+                                 <div className='shopping-cart-delete'>
+                                    <Link
+                                       to='#'
+                                       onClick={() =>
+                                          handleDeleteCartItem(item.id)
+                                       }
+                                    >
+                                       <FiX />
+                                    </Link>
+                                 </div>
+                              </li>
+                           ))
+                        )}
+                     </ul>
+                     {cart.length > 2 && (
+                        <div className='more-items-message bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 mt-2 rounded'>
+                           <span className='block'>
+                              Only the first 2 items are displayed.
+                           </span>
+                           <span className='block mt-1'>
+                              Open the cart to see all items.
+                           </span>
+                        </div>
+                     )}
+                     {cart.length > 0 && (
+                        <div className='shopping-cart-footer'>
+                           <div className='shopping-cart-total'>
+                              <h4>
+                                 Total <span>₹{subtotal}</span>
+                              </h4>
+                           </div>
+                           <div className='shopping-cart-button text-center'>
+                              <Link className='outline' to='/cart'>
+                                 View cart
+                              </Link>
+                           </div>
+                        </div>
+                     )}
+                  </div>
+               </div>
+
+               <div className='flex justify-center item-center  relative  cursor-pointer'>
+                  <DropdownMenu />
+               </div>
+               <div>
+                  <DarkNightMode />
+               </div>
             </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+         </div>
+         {/* Navbar section Start Here */}
+         {/* <Navbar /> */}
+         {/* Navbar section End Here */}
+         {location.pathname === '/' ? null : (
+            <div className='page-header breadcrumb-wrap mb-[30px]'>
+               <div className='container'>
+                  <div className='breadcrumb font-normal'>
+                     <Link to={'/'} rel='nofollow'>
+                        <FiHome className='inline mr-1' /> Home
+                     </Link>
+                     <FaAngleRight /> Pages <FaAngleRight /> {helmet}
+                  </div>
+               </div>
+            </div>
+         )}
+      </>
+   );
 };
 
 export default Header;
--- src/components/HomeSlider/HomeSlider.jsx
+++ src/components/HomeSlider/HomeSlider.jsx
@@ -2,10 +2,8 @@
 /* eslint-disable react/prop-types */
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import sliderImage1 from '../../assets/slider-1.png';
-import sliderImage2 from '../../assets/slider-2.png';
-import sliderImage5 from '../../assets/slider-5.png';
-import sliderImage6 from '../../assets/slider-6.png';
+import sliderImage1 from '../../assets/banner1.webp';
+import sliderImage2 from '../../assets/banner2.webp';
 
 import { IoIosArrowRoundForward, IoIosArrowRoundBack } from 'react-icons/io';
 import 'react-multi-carousel/lib/styles.css';
@@ -29,82 +27,51 @@ export default function HomeSlider() {
          slidesToSlide: 1,
       },
    };
-   
 
    return (
-     <section className='px-4 pt-4 pb-6'>
-       <div className='w-full overflow-hidden relative rounded-2xl'>
-         <Carousel
-           responsive={responsive}
-           showDots={true}
-           infinite
-           autoPlay
-           autoPlaySpeed={3000}
-           // removeArrowOnDeviceType={['tablet', 'mobile']}
-           customButtonGroup={<></>}
-           customLeftArrow={<CustomLeftArrow />}
-           customRightArrow={<CustomRightArrow />}
-           dotListClass='custom-dot-list'
-         >
-           <div>
-             <img src={sliderImage1} alt='slider' className='w-full' />
-             <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
-               <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                 Don’t miss amazing
-                 <br />
-                 grocery deals
-               </h2>
-               <p className='text-3xl text-gray-500 font-medium mb-12'>
-                 Save up to 50% off on your first order
-               </p>
-             </div>
-           </div>
-           <div>
-             <img src={sliderImage2} alt='slider' className='w-full' />
-             <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
-               <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                 Fresh Vegetables
-                 <br />
-                 Big discount
-               </h2>
-               <p className='text-3xl text-gray-500 font-medium mb-12'>
-                 Save up to 50% off on your first order
-               </p>
-             </div>
-           </div>
-           <div className='relative'>
-             <img src={sliderImage5} alt='slider' className='w-full' />
-             <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center flex-col z-10'>
-               <div className='text-center'>
-                 <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                   Don't miss
-                   <br />
-                   Amazing deals
-                 </h2>
-                 <p className='text-3xl text-gray-500 font-medium mb-12'>
-                   For your first order
-                 </p>
+      <section className='px-0 pt-4 pb-6'>
+         <div className='w-full overflow-hidden relative'>
+            <Carousel
+               responsive={responsive}
+               showDots={true}
+               infinite
+               autoPlay
+               autoPlaySpeed={3000}
+               // removeArrowOnDeviceType={['tablet', 'mobile']}
+               customButtonGroup={<></>}
+               customLeftArrow={<CustomLeftArrow />}
+               customRightArrow={<CustomRightArrow />}
+               dotListClass='custom-dot-list'
+            >
+               <div>
+                  <img src={sliderImage1} alt='slider' className='w-full' />
+                  <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
+                     <h2 className='text-7xl text-white font-bold mb-5'>
+                        Don’t miss amazing
+                        <br />
+                        grocery deals
+                     </h2>
+                     <p className='text-3xl text-white font-medium mb-12'>
+                        Save up to 50% off on your first order
+                     </p>
+                  </div>
                </div>
-             </div>
-           </div>
-           <div className='relative'>
-             <img src={sliderImage6} alt='slider' className='w-full' />
-             <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center flex-col z-10'>
-               <div className='text-center'>
-                 <h2 className='text-7xl text-gray-800 font-bold mb-5'>
-                   Fresh Vegetables
-                   <br />
-                   Big discount
-                 </h2>
-                 <p className='text-3xl text-gray-500 font-medium mb-12'>
-                   Get Free Delivery
-                 </p>
+               <div>
+                  <img src={sliderImage2} alt='slider' className='w-full' />
+                  <div className='absolute top-1/2 left-[-80px] transform -translate-x-72 -translate-y-1/2 text-center w-full z-10'>
+                     <h2 className='text-7xl text-white font-bold mb-5'>
+                        Fresh Vegetables
+                        <br />
+                        Big discount
+                     </h2>
+                     <p className='text-3xl text-white font-medium mb-12'>
+                        Save up to 50% off on your first order
+                     </p>
+                  </div>
                </div>
-             </div>
-           </div>
-         </Carousel>
-       </div>
-     </section>
+            </Carousel>
+         </div>
+      </section>
    );
 }
 
